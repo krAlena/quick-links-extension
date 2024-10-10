eabb3a991701652fcb00c459432071d0da8aff92
krAlena <krasnoshchoka@bk.ru>
2024-10-10 23:23:53 +0300

    add completed search as tag (design)

--- src/components/Common/SearchInput.js
+++ src/components/Common/SearchInput.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { Icon, Input } from 'semantic-ui-react';
 import "./SearchInput.sass";
 import SearchSvgIcon from '../Icons/SearchSvgIcon';
+import CloseSvgIcon from '../Icons/CloseSvgIcon';
 
 const SearchInput = ({searchAction}) => {
   const [strSearch, setStrSearch] = useState('');
+  const [isSearchComletedInput, setIsSearchComletedInput] = useState(false);
 
   const handleInputChange = (e) => {
     const searchValue = e.target.value;
@@ -20,6 +22,7 @@ const SearchInput = ({searchAction}) => {
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
+      setIsSearchComletedInput(true)
       if (typeof(searchAction) === "function"){
         searchAction(strSearch)
       }
@@ -33,20 +36,46 @@ const SearchInput = ({searchAction}) => {
   };
 
   const handleClearSearch = () => {
-    setStrSearch('')
+    let searchValue = '';
+    setStrSearch(searchValue);
+    setIsSearchComletedInput(false);
+
+    if (typeof(searchAction) === "function"){
+      searchAction(searchValue)
+    }
   }
 
   return (
-    <Input
-      icon={
-        <SearchSvgIcon className={(strSearch !== '') ? 'icon search' : 'icon search gray'} onClick={handleSearchIconClick}/>
+    <>
+      <Input
+        icon={
+          <SearchSvgIcon className={(strSearch !== '') ? 'icon search' : 'icon search gray'} onClick={handleSearchIconClick}/>
+        }
+        placeholder='Search...'
+        size='small'
+        value={strSearch}
+        disabled={isSearchComletedInput}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress} // Handle Enter key press
+      />
+      {
+        isSearchComletedInput
+          ? <div className="tags-list">
+              <div className='active tag with-del'>
+                <div className="tag__text">
+                  {strSearch}
+                </div>
+                <div className="tag__del"
+                    onClick={handleClearSearch}
+                >
+                  <CloseSvgIcon className="icon btn close hover-blue small"/>
+                </div>
+              </div>
+            </div>
+          : null
       }
-      placeholder='Search...'
-      size='small'
-      value={strSearch}
-      onChange={handleInputChange}
-      onKeyPress={handleKeyPress} // Handle Enter key press
-    />
+    </>
+
   );
 };
 
